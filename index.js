f40a7c01bcae0c3e524ec9186c3035563540643d
Roopa Suresh <roopasuresh431@gmail.com>
2022-03-16 11:52:13 +0530

    bankserver

---
+++ index.js
@@ -0,0 +1,140 @@
+//first step is to import express.
+//for this require is used. require is a function or method.
+//its format inside bracket is id:string ie, give name of the library to be imported in single quotes
+//assign it to a constant
+const express=require('express')
+//importing dataservice.js
+const dataService=require('./services/data.service')
+
+
+
+//step 2 to create an app using express
+//its format is const app=the variable in which express has been assigned to with function call ie()
+const app=express()
+
+
+
+//to parse json
+app.use(express.json())
+
+
+
+
+
+//step 4 resolve http request from client (using express)
+//GET-to read data
+//    / means- in which url its output should be resolved and shown.if it is to be shown in localhost 3000 use /
+//the steps used for resolving that request should be given in a callback function
+//CALLBACK FUNCTION SHOULD ALWAYS BE IN ARROW FUNCTION
+//request and response are the two variables inside callback function. you should always use that
+app.get('/',(req,res)=>{
+    // res.send("It's a get method")
+    //every response has a status. status decides if the request is a success or not
+    //response with a status in the range 200 means its a success
+       //response with a status in the range 100 means its a information
+     //response with a status in the range 300 means its a redirection
+    //response with a status in the range 400 means its a error from client
+     //response with a status in the range 500 means its a error in server side
+   
+    //to change the status forcefully(usually it is 200 ok . you can change it to any value)
+    res.status(401).send("It's a get method")
+})
+
+
+//POST-to create data
+app.post('/',(req,res)=>{
+    res.send("It's a post method")
+})
+
+
+//PuT-to update/modify data
+app.put('/',(req,res)=>{
+    res.send("It's a put method")
+})
+
+
+//PATCH-to update partially data
+app.patch('/',(req,res)=>{
+    res.send("It's a patch method")
+})
+
+
+//delete-to delete data
+app.delete('/',(req,res)=>{
+    res.send("It's a delete method")
+})
+
+
+
+
+
+// //bank app- API
+// //1 register API
+// //req has 2 parts.head angd body. the one from client will be in request's body. authentication etc will be in req's head
+// app.post('/register',(req,res)=>{
+//     //calling register function(first import dataservice.js in index.js)
+// const result=dataService.register(req.body.acno,req.body.pswd,req.body.uname) //variable name can be different during calling but it should be in the same order as that of function definition
+// //this variable should be same as that in json of thunderclient
+// if(result){
+// res.send("Registered successfully")
+// }
+// else{
+//     res.send("Already exist please log in")
+// }
+
+// })
+
+
+// //bank app- API
+// //1 register API (after defining register function based on status)
+// //req has 2 parts.head angd body. the one from client will be in request's body. authentication etc will be in req's head
+// app.post('/register',(req,res)=>{
+//     //calling register function(first import dataservice.js in index.js)
+// const result=dataService.register(req.body.acno,req.body.pswd,req.body.uname) //variable name can be different during calling but it should be in the same order as that of function definition
+// //this variable should be same as that in json of thunderclient
+// //now no need of this if and else case(ie next steps) since we have already defined it based on status in regiser definition
+
+// // if(result){
+// // res.send("Registered successfully")
+// // }
+// // else{
+// //     res.send("Already exist please log in")
+// // }
+
+// res.status(result.statusCode).json(result)
+// })
+
+
+//bank app- API
+//1 register API
+app.post('/register',(req,res)=>{
+const result=dataService.register(req.body.acno,req.body.pswd,req.body.uname)  
+res.status(result.statusCode).json(result)
+})
+
+//2 login API
+app.post('/login',(req,res)=>{
+    const result=dataService.login(req.body.acno,req.body.password)
+    res.status(result.statusCode).json(result)
+})
+
+//3 deposit API
+app.post('/deposit',(req,res)=>{
+    const result=dataService.deposit(req.body.acno,req.body.password,req.body.amt)
+    res.status(result.statusCode).json(result)
+})
+
+
+
+
+
+
+
+//step 3 setup the port number
+///give the port number from 3000 and dont give 4200 as it has been already used. 
+//if 4200 is used again it will show the msg port already in use
+//the bracket after 3000 is callback function ie, function inside a function.it is usually not necessary. only the port no is enough
+//format is app.listen(port no,(callback function)=>what you need to show)
+app.listen(3000,()=>{
+    console.log("server started at port no: 3000");
+})
---
+++ services/data.service.js
@@ -0,0 +1,148 @@
+database = {
+  1000: { acno: 1000, uname: "Neer", password: 1000, balance: 5000, transaction: [] },
+  1001: { acno: 1000, uname: "Vyom", password: 1001, balance: 5000, transaction: [] },
+  1002: { acno: 1000, uname: "Laisha", password: 1002, balance: 5000, transaction: [] }
+
+}
+
+// //register define
+//  const register=(acno, pswd, uname)=> {
+
+//     if (acno in database) {
+//       return false
+//     }
+//     else {
+
+//       database[acno] = {
+//         acno,
+//         uname,
+//         password: pswd, //key:value
+//         balance: 0,
+//         transaction:[]
+//       }
+//       console.log(database)
+//       return true
+//     }
+//   }
+
+
+//register defining based on status
+const register = (acno, pswd, uname) => {
+
+  if (acno in database) {
+    return {
+      statusCode: 422,
+      status: false,
+      message: "User already exist please login"
+    }
+  }
+  else {
+
+    database[acno] = {
+      acno,
+      uname,
+      password: pswd, //key:value
+      balance: 0,
+      transaction: []
+    }
+    console.log(database)
+    // return true
+    return {
+      statusCode: 200,
+      status: true,
+      message: "Successfully registered"
+
+    }
+  }
+}
+
+
+
+
+//login
+const login = (acno, password) => {
+
+  if (acno in database) {
+    if (password == database[acno]["password"]) {
+      currentAcno = acno
+      //storing uname into a variable
+      currentUname = database[acno]["uname"]
+      // return true
+      return {
+        statusCode: 200,
+        status: true,
+        message: "Successfully logged in",
+        currentAcno,
+        currentUname
+      }
+    }
+    else {
+      // return false
+      return {
+        statusCode: 422,
+        status: false,
+        message: "Incorrect password"
+
+      }
+    }
+  }
+  else {
+    // return false
+    return {
+      statusCode: 422,
+      status: false,
+      message: "User does not exist"
+
+    }
+  }
+}
+
+
+//deposit definition
+const deposit = (acno, password, amt)=> {
+  var amount = parseInt(amt)
+
+  if (acno in database) {
+    if (password == database[acno]["password"]) {
+      database[acno]["balance"] += amount
+      database[acno]["transaction"].push({
+        //key:value
+        amount: amount,
+        type: "CREDIT"
+      })
+      return {
+        statusCode: 200,
+        status: true,
+        message: amount + " successfully deposited and new balance is " + database[acno]["balance"]
+      }
+    }
+    else {
+      return {
+        statusCode: 422,
+        status: false,
+  message:"Incorrect password"
+      }
+    }
+  }
+  else {
+    return {
+      statusCode: 422,
+      status: false,
+      message: "User does not exist"
+ 
+    }
+  }
+}
+
+
+
+
+
+
+
+//exporting
+module.exports = {
+  register,
+  login,
+  deposit
+}
