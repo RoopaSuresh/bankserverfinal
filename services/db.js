59d713f64b68ac80cf8db6231f6b2e0ef2baa681
Roopa Suresh <roopasuresh431@gmail.com>
2022-03-22 11:39:06 +0530

    new modifications

---
+++ services/db.js
@@ -0,0 +1,24 @@
+//db.js - to give mongo db connection details
+
+//mongoose import
+const mongoose=require('mongoose')
+
+//state connection string
+mongoose.connect('mongodb://localhost:27017/bank',{
+    useNewUrlParser:true
+})
+
+//model creation- model's name should be in singular and initial letter should be in capital leteter
+const User=mongoose.model('User',{
+    acno: Number,
+     uname: String,
+      password: String,
+       balance: Number,
+        transaction: []
+})
+
+
+//export model ie User
+module.exports={
+    User
+}
